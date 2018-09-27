77cff9d2dfbbffa700902418718c307981a6949d
Pravin Bashyal <pravin.bashyal@gmail.com>
2018-09-27 12:41:25 +0200

    digits version 2

--- digits.py
+++ digits.py
@@ -11,16 +11,4 @@ print(computerGuessedNumber)
 
 while True:
     userGuessedNumber = input('guess the three digit number: ')
-    userGuessedNumber = list(userGuessedNumber)
-    numberOfCorrectDigits = 0
-    numberOfCorrectPositions = 0
-    for c_pos, c_digit in enumerate(computerGuessedNumber):
-        for u_pos, u_digit in enumerate(userGuessedNumber):
-            if (str(c_digit ) == str(u_digit)):
-                numberOfCorrectDigits = numberOfCorrectDigits + 1
-                if(c_pos == u_pos):
-                    numberOfCorrectPositions = numberOfCorrectPositions + 1
-    print('you guessed \n' + str(numberOfCorrectDigits )+ ' digits correctly \n' + str(numberOfCorrectPositions) + ' positions correctly')
-    if(numberOfCorrectDigits == 3 and numberOfCorrectPositions == 3) :
-        print('you win')
-        break
+    conaosdifjaosidjf
